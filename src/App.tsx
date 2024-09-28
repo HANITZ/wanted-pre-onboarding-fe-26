f1ffa07f5f0cc346a4e5dfeb31b2ef2baf324c7e
장한울 <swingjhw@naver.com>
2024-09-28 18:07:08 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getMockData, MockData } from "./mock/product";
 
 function App() {
+  const [products, setProducts] = useState<MockData[]>([]);
+  const [page, setPage] = useState(1);
+  const [end, setEnd] = useState(false);
+  useEffect(() => {
+    getMockData(page).then(({ datas, isEnd }) => {
+      setProducts((prev) => [...prev, ...datas]);
+      setEnd(isEnd);
+    });
+  }, [page]);
+  console.log(products);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 25 }}>
+      <ul>
+        {products &&
+          products.map((product) => (
+            <li key={product.productId}>
+              <span>{product.productName}</span>
+              <span>{product.price}</span>
+            </li>
+          ))}
+        <li>
+          <span>{}</span>
+        </li>
+      </ul>
     </div>
   );
 }
